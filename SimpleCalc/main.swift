f8d6fd07d62656c361966674cec0fa5165ab9e8e
Naruth Kongurai <naruth1212@gmail.com>
2017-10-05 18:18:57 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Naruth Kongurai on 10/5/17.
+//  Copyright © 2017 Naruth Kongurai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
